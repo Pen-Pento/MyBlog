d2009ca79275c998c1d46d746880e0a66520e477
EC2 Default User <ec2-user@ip-172-31-13-175.us-east-2.compute.internal>
2018-08-05 08:15:38 +0000

    new-column

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,16 +53,13 @@ class MessagesController < ApplicationController
 
   # DELETE /messages/1
   # DELETE /messages/1.json
-  def create
-    @message = Message.new(message_params)
-    respond_to do |format|
-    if @message.save
-      format.html { redirect_to messages_url, notice: 'Message was successfully created.' }
-      format.json { render :show, status: :created, location: @message }
-    
-    end
-  end
-end
+  def destroy
+ @message.destroy
+ respond_to do |format|
+ format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
+ format.json { head :no_content }
+ end
+ end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_message
@@ -71,6 +68,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
-      params.require(:message).permit(:title, :message, :name)
+      params.require(:message).permit(:title, :message, :name, :value)
     end
 end
